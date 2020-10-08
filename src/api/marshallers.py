48225b395e6d5e36c8599c9e43be0398e7265e51
nomilkinmyhome <vyckluchi75@gmail.com>
2020-10-08 22:53:46 +0300

    Add todo_detail_fields

--- src/api/marshallers.py
+++ src/api/marshallers.py
@@ -2,13 +2,20 @@ from flask_restx import fields
 
 from .blueprint import rest_api
 
+todo_detail_fields = rest_api.model('TodoDetail', {
+    'id': fields.Integer(),
+    'title': fields.String(),
+    'description': fields.String(),
+    'is_done': fields.Boolean()
+})
+
 user_detail_fields = rest_api.model('UserDetail', {
     'id': fields.Integer(),
     'email': fields.String(),
+    'todos': fields.Nested(todo_detail_fields),
     'is_activated': fields.Boolean(),
     'is_admin': fields.Boolean()
 })
-
 
 tokens_fields = rest_api.model('Tokens', {
     'access_token': fields.String(),
