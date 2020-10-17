d329d8327063d1b2d67bc5387b5c07a520fd1d77
nomilkinmyhome <vyckluchi75@gmail.com>
2020-10-17 14:11:00 +0300

    Add some tests

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.app import create_app
+from src.models import db
+from src.models.user import User
+from src.models.todo import Todo
+
+
+@pytest.fixture(scope='session')
+def client():
+    app = create_app()
+
+    with app.test_client() as client:
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+
+            yield client
+
+
+@pytest.fixture
+def user():
+    user = User(email='user@email.com', is_activated=True)
+    user.set_password('1')
+    db.session.add(user)
+    db.session.commit()
+
+    yield user
---
+++ tests/integration/test_todo.py
@@ -0,0 +1,22 @@
+from use_cases.todo import create_todo, update_todo
+
+
+def test_create_todo(client, user):
+    payload = {
+        'title': 'test todo',
+        'description': 'very long and useful description',
+    }
+    todo = create_todo(user.id, payload)
+
+    assert todo.id == 1
+    assert todo.title == payload['title']
+
+
+def test_update_todo(client):
+    todo_id = 1
+    old_description = 'very long and useful description'
+    payload = {'title': 'new title'}
+    todo = update_todo(todo_id, payload)
+
+    assert todo.title == payload['title']
+    assert todo.description == old_description
